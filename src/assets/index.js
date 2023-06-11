d3450cbc3d2236ba317de24ca2561975ce12e55c
lokeshirl <lokeshkumarjha07+github@gmail.com>
2023-06-11 16:37:07 +0530

    added code-editor-compiler

---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+import avatarAnkur from './avatar-ankur.jpg';
+import avatarLokesh from './avatar-lokesh.jpg';
+import avatarMukund from './avatar-mukund.png';
+import avatarAshutosh from './avatar-ankur.jpg';
+import cppIcon from './cpp.png';
+import pythonIcon from './python.png';
+import cIcon from './c.png';
+import javaIcon from './java.png';
+import javascriptIcon from './javascript.png';
+
+export {
+  avatarAnkur,
+  avatarLokesh,
+  avatarMukund,
+  avatarAshutosh,
+  cppIcon,
+  pythonIcon,
+  cIcon,
+  javaIcon,
+  javascriptIcon,
+};
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,11 +1,96 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import CodeEditor from '@monaco-editor/react';
+import files from '../defaultStubs';
+import {
+  cppIcon,
+  pythonIcon,
+  javaIcon,
+  javascriptIcon,
+  cIcon,
+} from '../assets';
 
 const Editor = () => {
+  const [fileName, setFileName] = useState('cpp');
+  const [inputLanguage, setInputLanguage] = useState('cpp');
+  const [outputLanguage, setOutputLanguage] = useState('python');
+  const [code, setCode] = useState('');
+  const [input, setInput] = useState('0');
+  const [output, setOutput] = useState('');
+  const [languageIcon, setLanguageIcon] = useState(cppIcon);
+  const editorRef = useRef(null);
+
+  const file = files[fileName];
+  const dataForCompiler = {
+    code: code,
+    language: inputLanguage,
+    input: input,
+  };
+
+  const handleEditorDidMount = (editor, monaco) => {
+    editorRef.current = editor;
+  };
+
+  const handleSelectIcon = (e) => {
+    switch (e.target.value) {
+      case 'c':
+        setLanguageIcon(cIcon);
+        setInputLanguage('c');
+        setFileName('c');
+        break;
+      case 'python3':
+        setLanguageIcon(pythonIcon);
+        setFileName('python');
+        setInputLanguage('python');
+        break;
+      case 'java':
+        setLanguageIcon(javaIcon);
+        setFileName('java');
+        setInputLanguage('java');
+        break;
+      case 'javascript':
+        setLanguageIcon(javascriptIcon);
+        setFileName('javascript');
+        setInputLanguage('javascript');
+        break;
+      default:
+        setLanguageIcon(cppIcon);
+        setFileName('cpp');
+        setInputLanguage('cpp');
+        break;
+    }
+    console.log(e.target.value);
+  };
+
+  const getEditorValue = () => {
+    setCode(editorRef.current.getValue());
+    console.log(editorRef.current.getValue());
+  };
+
   return (
     // editor
     <>
-      <header>Header</header>
+      <header>
+        <nav className="container ml-5">
+          {/* language logo btn */}
+          <button className="mr-5">
+            <img
+              src={languageIcon}
+              className="image inline-block"
+              alt={`${inputLanguage} icon`}
+            />
+          </button>
+          {/* language logo drop-down btn */}
+          <button className="mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0 mr-5">
+            <select onChange={handleSelectIcon}>
+              <option value={'cpp'}>C++</option>
+              <option value={'c'}>C</option>
+              <option value={'java'}>Java</option>
+              <option value={'python3'}>Python 3</option>
+              <option value={'javascript'}>Javascript</option>
+            </select>
+          </button>
+        </nav>
+      </header>
       <section id="editor">
         {/* main--container */}
         <div className="flex flex-row space-x-4 items-start px-4 py-4">
@@ -14,7 +99,15 @@ const Editor = () => {
             className="flex flex-col w-full h-full justify-start items-end"
             id="code-editor"
           >
-            <CodeEditor height="85vh" width={`100%`} />
+            <CodeEditor
+              height="85vh"
+              width={`100%`}
+              theme="vs-dark"
+              path={file.name}
+              defaultLanguage={file.language}
+              defaultValue={file.value}
+              onMount={handleEditorDidMount}
+            />
           </div>
 
           {/* input-output */}
@@ -22,7 +115,7 @@ const Editor = () => {
             className="right-container flex flex-shrink-0 w-[30%] flex-col"
             id="std-input-output"
           >
-            <h1 class="font-bold text-xl bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-slate-700 mb-2">
+            <h1 className="font-bold text-xl bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-slate-700 mb-2">
               Output
             </h1>
             <div
@@ -33,11 +126,22 @@ const Editor = () => {
               <textarea
                 rows="5"
                 placeholder="Custom input"
-                class="focus:outline-none w-full border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white mt-2"
+                className="focus:outline-none w-full border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white mt-2"
               ></textarea>
-              <button class="mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0 ">
-                Compile and Execute
-              </button>
+              <div>
+                <button
+                  className="mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0 mr-5"
+                  onClick={() => getEditorValue()}
+                >
+                  Explain code
+                </button>
+                <button
+                  className="mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0 "
+                  onClick={() => getEditorValue()}
+                >
+                  Compile and Execute
+                </button>
+              </div>
             </div>
           </div>
         </div>
